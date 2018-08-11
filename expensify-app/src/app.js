b79f93aa519a09a4623330c3ec8820f767565ce0
Hashir-Qureshi <qureshihashir@yahoo.com>
2018-08-11 02:37:52 -0400

    connected react and redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
+const store = configureStore();
 
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Gas bill" }));
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(setTextFilter("water"));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter("bill"));
+}, 3000);
+
+const state = store.getState();
+
+console.log(getVisibleExpenses(state.expenses, state.filters));
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
