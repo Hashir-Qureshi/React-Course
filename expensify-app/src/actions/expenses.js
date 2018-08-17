2aea7e87b9ca57cfb44c2f01dafead5b571609e8
Hashir-Qureshi <qureshihashir@yahoo.com>
2018-08-16 21:14:06 -0400

    Finished redux async

--- expensify-app/src/actions/expenses.js
+++ expensify-app/src/actions/expenses.js
@@ -1,27 +1,55 @@
 import uuid from "uuid";
+import database from "../firebase/firebase";
+// components call action generator
+// action generator returns function
+// component dispatches function (?)
+// function runs (has the ability to dispatch other actions and do whatever it wants)
 
 // ADD_EXPENSE
-export const addExpense = ({
-  description = "",
-  note = "",
-  amount = 0,
-  createdAt = 0
-} = {}) => ({
+export const addExpense = expense => ({
   type: "ADD_EXPENSE",
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 });
 
+export const startAddExpense = (expenseData = {}) => {
+  return dispatch => {
+    const {
+      description = "",
+      note = "",
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    database
+      .ref("expenses")
+      .push(expense)
+      .then(ref => {
+        dispatch(
+          addExpense({
+            id: ref.key,
+            ...expense
+          })
+        );
+      });
+  };
+};
+
 // REMOVE_EXPENSE
-export const removeExpense = ({ id } = {}) => ({
+export const removeExpense = id => ({
   type: "REMOVE_EXPENSE",
   id
 });
+
+export const startRemoveExpense = id => {
+  return dispatch => {
+    database
+      .ref(`expenses/${id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense(id));
+      });
+  };
+};
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
@@ -29,3 +57,42 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+export const startEditExpense = (id, updates) => {
+  return dispatch => {
+    database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
+};
+
+// Set_EXPENSES
+export const setExpenses = expenses => ({
+  type: "SET_EXPENSES",
+  expenses
+});
+
+// added second return and returning promise for test cases or
+// if the code calling the function needs to do something after
+export const startSetExpenses = () => {
+  return dispatch => {
+    return database
+      .ref("expenses")
+      .once("value")
+      .then(snapshot => {
+        const expenses = [];
+
+        snapshot.forEach(childSnapshot => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          });
+        });
+
+        dispatch(setExpenses(expenses));
+      });
+  };
+};
--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -3,23 +3,49 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
+import { startSetExpenses } from "./actions/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "./firebase/firebase";
+import selectExpensesTotal from "./selectors/expenses-total";
+
+const expenses = [
+  {
+    id: "1",
+    description: "Gum",
+    note: "",
+    amount: 195,
+    createdAt: 0
+  },
+  {
+    id: "2",
+    description: "Rent",
+    note: "",
+    amount: 109500,
+    createdAt: 0
+  },
+  {
+    id: "3",
+    description: "Credit Card",
+    note: "",
+    amount: 4500,
+    createdAt: 0
+  }
+];
+
+const total = selectExpensesTotal(expenses);
+
+console.log(total);
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
+ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-ReactDOM.render(jsx, document.getElementById("app"));
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById("app"));
+});
