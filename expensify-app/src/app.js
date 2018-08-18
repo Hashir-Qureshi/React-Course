4a3437aa7993e7d0b782df183913b89c4624f7f3
Hashir Qureshi <qureshihashir@yahoo.com>
2018-08-17 21:37:10 -0400

    Started Auth

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,41 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import AppRouter from "./routers/AppRouter";
+import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { startSetExpenses } from "./actions/expenses";
+import { login, logout } from "./actions/auth";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
-import "./firebase/firebase";
-import selectExpensesTotal from "./selectors/expenses-total";
-
-const expenses = [
-  {
-    id: "1",
-    description: "Gum",
-    note: "",
-    amount: 195,
-    createdAt: 0
-  },
-  {
-    id: "2",
-    description: "Rent",
-    note: "",
-    amount: 109500,
-    createdAt: 0
-  },
-  {
-    id: "3",
-    description: "Credit Card",
-    note: "",
-    amount: 4500,
-    createdAt: 0
-  }
-];
-
-const total = selectExpensesTotal(expenses);
-
-console.log(total);
+import { firebase } from "./firebase/firebase";
 
 const store = configureStore();
 
@@ -44,8 +16,28 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
+
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("app"));
+    hasRendered = true;
+  }
+};
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById("app"));
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push("/");
+  }
 });
